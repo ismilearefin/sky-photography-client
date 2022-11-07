d6c3337c5cc9e09a0ce85b7df0969564c8b197c6
ismilearefin <arefinismile@gmail.com>
2022-11-08 03:20:38 +0600

    add react photo view

--- src/Components/Pages/AllService/AllService.js
+++ src/Components/Pages/AllService/AllService.js
@@ -1,14 +1,62 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import { Link, useLoaderData } from 'react-router-dom';
+
 
 const AllService = () => {
     const allservices = useLoaderData();
     console.log(allservices)
     return (
         <div>
-            
+            <div>
+            <h1 className='text-3xl italic my-2'>My services</h1>
+            <div className='flex gap-4 justify-center flex-wrap my-14'>
+            {
+                allservices.map(serviceCard => <ShowAllCard 
+                    serviceCard={serviceCard}
+                    key={serviceCard._id}
+                ></ShowAllCard>)
+            }
+        </div>
+        </div>
         </div>
     );
 };
+
+
+function ShowAllCard({serviceCard}){
+    const {img, rating,price,name,dis,_id} = serviceCard
+    return(
+        <div >
+            <div className="card w-96 bg-base-100 shadow-xl ">
+            <figure>
+                <PhotoProvider>
+                    <PhotoView src={img}>
+                    <img src={img} alt="Shoes" />
+                    </PhotoView>
+                </PhotoProvider>
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{name}</h2>
+                <p>{dis.length > 100 ? dis.slice(0, 99) + '...' : dis}</p>
+                <div className='flex'>
+                    <p>Price : {price}</p>
+                    <p>Rating : {rating}</p>
+                </div>
+                <div className="card-actions justify-end">
+                <Link to={`/services/${_id}`} className="btn btn-ghost w-full shadow-md">VIEW DETAILS</Link>
+                </div>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+
+
+
+
+
+
 
 export default AllService;
--- src/Components/Pages/Home/Body/Service/Service.js
+++ src/Components/Pages/Home/Body/Service/Service.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 import { Link } from 'react-router-dom';
 
 const Service = () => {
@@ -28,12 +29,18 @@ const Service = () => {
 };
 
 
-export function Showevents({serviceCard}){
+function Showevents({serviceCard}){
     const {img, rating,price,name,dis,_id} = serviceCard
 return(
     <div >
-        <div className="card w-96 bg-base-100 shadow-xl image-full">
-        <figure><img src={img} alt="Shoes" /></figure>
+        <div className="card w-96 bg-base-100 shadow-xl ">
+        <figure>
+            <PhotoProvider>
+                <PhotoView src={img}>
+                <img src={img} alt="Shoes" />
+                </PhotoView>
+            </PhotoProvider>
+        </figure>
         <div className="card-body">
             <h2 className="card-title">{name}</h2>
             <p>{dis.length > 100 ? dis.slice(0, 99) + '...' : dis}</p>
@@ -42,7 +49,7 @@ return(
                 <p>Rating : {rating}</p>
             </div>
             <div className="card-actions justify-end">
-            <Link to={`/services/${_id}`} className="btn btn-ghost w-full">VIEW DETAILS</Link>
+            <Link to={`/services/${_id}`} className="btn btn-ghost w-full shadow-md">VIEW DETAILS</Link>
             </div>
         </div>
         </div>
