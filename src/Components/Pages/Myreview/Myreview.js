d79660735aaf06e3530babddebbf051fa7ec0868
ismilearefin <arefinismile@gmail.com>
2022-11-08 22:23:39 +0600

    load user comments in my review page

--- src/Components/Pages/Myreview/Myreview.js
+++ src/Components/Pages/Myreview/Myreview.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { Authcontext } from '../../../Usercontext/Usercontext';
 
 const Myreview = () => {
+    const {user} = useContext(Authcontext);
+    const [userComment, setuserComment] = useState([])
+    useEffect(()=>{
+        fetch(`http://localhost:5000/allcomments/user?email=${user?.email}`)
+        .then(res => res.json())
+        .then(data => setuserComment(data))
+    },[user?.email])
     return (
         <div>
-            my reviews
+            my All Reviews {userComment.length}
         </div>
     );
 };
